fe997617c225dc56ee489159ab52dc0263c4b78d
nwhahn <nwhahn@buffalo.edu>
2016-12-09 19:38:09 -0500

    Update Amazeing.py

--- Amazeing.py
+++ Amazeing.py
@@ -3,8 +3,13 @@ import maze_vision_algo as mvsp
 import os
 import time
 import spur
+import string
 
 def main():
+   f=open('config.txt','r')
+   for line in f.readlines:
+       strpi=line
+       strpi.replace('\n','')
    rad = it.trans_main()
 	#print(rad)
    p = mvsp.mvsp(rad)
@@ -13,7 +18,7 @@ def main():
    #os.system('ssh pi@192.168.1.17 | python maze-vision-driving_dev/Florence/Solve_Path')
    #this.run(['cd maze-vision-driving_dev/Florence'])
    #this.run(['python Solve_Path '+str(p)])
-   os.system('ssh pi@192.168.1.17 python maze-vision-driving_dev/Florence/Solve_Path.py '+str(p))
+   os.system('ssh pi@'+strpi+' python maze-vision-driving_dev/Florence/Solve_Path.py '+str(p))
    print('done!')
 
 
